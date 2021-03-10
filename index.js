75c94373ad6539dc1b0deef1a2e2d57a1e449a20
edilbertojj65 <edilbertojj65@gmail.com>
2021-03-10 17:40:27 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Follow along with the examples here
+
+function weCanDo(name, age , message ) {
+
+    let isAdult=false, canWork=false, canEnlist=false, canDrink=false;
+
+    if (age >= 21) {
+        canWork = true;
+        canEnlist = true;
+        isAdult = true;
+        canDrink = true;
+        console.log(`${name}`);
+        message = ` With: ${age} years, you can drink and work`;
+
+      } else if (age >= 18) {
+        canWork = true;
+        canEnlist = true;
+        isAdult = true;
+      } else if (age >= 16) {
+        canWork = true;
+      }
+      return `${message}`;
+    }
+
+  console.log(weCanDo("Jhon", "35", ""));
